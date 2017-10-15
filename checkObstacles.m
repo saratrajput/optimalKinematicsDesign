f3b9dec24bb3081c4cc6d53ca893e8a5a58d89ab
saratrajput <pattarsuraj@gmail.com>
2017-10-15 19:40:17 +0200

    15OctSun

--- checkObstacles.m
+++ checkObstacles.m
@@ -6,8 +6,10 @@ a = ((XB-XA)^2 + (YB-YA)^2);
 b = (2*XA*(XB-XA) - 2*XC*(XB-XA) + 2*YA*(YB-YA) - 2*YC*(YB-YA));
 c = XA^2 + XC^2 - 2*XA*XC + YA^2 + YC^2 - 2*YA*YC - R^2;
 delta = b^2 - 4*a*c;
+
 if (delta>=0) t1 = (-b - sqrt(delta))/(2*a);
 t2 = (-b + sqrt(delta))/(2*a);
+
 if ((t1 >= 0 && t1 <= 1) || (t2 >= 0 && t2 <= 1))
 cols = 1;
 end
--- findBase.m
+++ findBase.m
@@ -1,9 +1,11 @@
 function [ xBase, yBase ] = findBase( L1, L2, theta1, theta2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-xBase = xEnd - (L1 * cos(theta1a) + L2 * cos(theta1a + theta2a));
+xEnd = 300;
+yEnd = 400;
+xBase = xEnd - (L1 * cos(theta1) + L2 * cos(theta1 + theta2));
 
-yBase = yEnd - (L1 * sin(theta1a) + L2 * sin(theta1a + theta2a));
+yBase = yEnd - (L1 * sin(theta1) + L2 * sin(theta1 + theta2));
 
 end
 
